6202fc6efe521ce1669c10115bd9d5afd3a03790
KaMa <maid.kanuric>
2025-04-23 22:03:51 +0200

    Extend WatchlistRepository class with basics

--- src/main/java/at/ac/fhcampuswien/fhmdb/db/WatchlistRepository.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/db/WatchlistRepository.java
@@ -1,4 +1,26 @@
 package at.ac.fhcampuswien.fhmdb.db;
 
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import com.j256.ormlite.dao.Dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WatchlistRepository {
+    private Dao<WatchlistMovieEntity, Long> dao;
+
+    public List<WatchlistMovieEntity> getWatchlist() {
+        // TEMPORARY, TODO: Add proper functionality
+        return new ArrayList<>();
+    }
+
+    int addToWatchlist(WatchlistMovieEntity movie) {
+        // TEMPORARY, TODO: Add proper functionality
+        return -1;
+    }
+
+    int removeFromWatchlist(String aplid) {
+        // TEMPORARY, TODO: Add proper functionality
+        return -1;
+    }
 }
